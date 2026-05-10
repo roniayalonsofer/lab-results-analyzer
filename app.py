f977bc13e6d202a17e08da81569b761af8d3c605
r5901 <adamaisraelpolls@gmail.com>
2026-05-10 15:08:42 +0300

    auto: 2026-05-10 15:08

--- app.py
+++ app.py
@@ -439,7 +439,9 @@ try:
     from core.excel_output import LabReportExcel
     from core.word_output  import (LabReportWord, parse_als_file, build_word_report,
                                    load_threshold_file, get_tier1_col, tier1_label)
+    from core.xrf_output   import build_xrf_simple_excel
     from parsers import get_parser, auto_detect_category, auto_detect_lab
+    from parsers.soil.xrf import XRFSoilParser
     _tm_py    = os.path.join(TOOL_DIR, 'core', 'threshold_manager.py')
     _tm_mtime = os.path.getmtime(_tm_py)
     tm = load_threshold_manager(_tm_mtime)
