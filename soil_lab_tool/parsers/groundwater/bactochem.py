f9d613302133f41591a156f61ba577abeaaa811d
r5901 <adamaisraelpolls@gmail.com>
2026-05-10 16:49:51 +0300

    auto: 2026-05-10 16:49

--- soil_lab_tool/parsers/groundwater/bactochem.py
+++ soil_lab_tool/parsers/groundwater/bactochem.py
@@ -333,7 +333,7 @@ class BactochemGroundwaterParser(BaseParser):
     """
 
     LAB_NAME = "בקטוכם"
-    ANALYSIS_TYPES = ["GW_VOC", "GW_FIELD_PARAMS"]
+    ANALYSIS_TYPES = ["GW_VOC", "GW_FIELD_PARAMS", "GW_PFAS", "GW_MICROBIOLOGY"]
 
     # Named columns used by Bactochem
     COL_COMPOUND = "רכיב"
